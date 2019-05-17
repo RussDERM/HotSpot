82d4e087e31fdff0d562fa150ecf7ac820cfe905
Russ <russ.dermody@gmail.com>
2019-05-17 13:35:17 -0700

    Added folder structure and public pages

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ var path = require('path');
 // grap express
 var app = express();
 // PORT to use, needs something else. 
-var PORT = 3000;
+var PORT = process.env.POT || 8080;
 // express parameters
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // HEY! LISTEN!
+
+
+
+
+
+
+
+
+
 app.listen(PORT, function () {
   console.log("HEY! LISTEN! ON PORT: " + PORT);
 });
